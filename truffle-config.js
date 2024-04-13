fb74689f5f0f25cfb120f689ad04007a44cdf63b
itzarnavbro <141902697+itzarnavbro@users.noreply.github.com>
2024-04-13 12:12:10 +0530

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777",
+      gas: 6721975,
+      from: "0x21c526D25D377e9dF33d4ebC35891FC83f3B8A87",
+    },
+  },
+  contracts_directory: "./testing/",
+  contracts_build_directory: "./src/build/",
+  compilers: {
+    solc: {
+      version: "0.6.6",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 100,
+        },
+      },
+    },
+  },
+};
